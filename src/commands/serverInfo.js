e8afc52dc05306917bc5d5421e2c3871b1ce7c1b
Muntasir <muntasirenamalhuda@gmail.com>
2021-05-27 17:37:12 +0100

    updated serverinfo structure

--- src/commands/serverInfo.js
+++ src/commands/serverInfo.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 const PREFIX = process.env.PREFIX;
 
 const serverInfo = (message, client) => {
@@ -7,14 +9,58 @@ const serverInfo = (message, client) => {
 			.substring(PREFIX.length)
 			.split(/\s+/); //this is a regular expression which eliminates multiple whitespaces in the command
 
+		const { guild } = message;
+		const { name, region, memberCount, roles, owner } = guild;
+		const icon = guild.iconURL();
+
+		// console.log(name, region, memberCount, icon);
+		console.log();
+
+		const infoEmbed = new Discord.MessageEmbed()
+			.setColor('#FF4454')
+			.setTitle(name)
+			.setDescription(`Info about ${name}`)
+			.setThumbnail(icon)
+			.addFields(
+				// { name: '\u200B', value: '\u200B' },
+				{
+					name: 'Server Owner',
+					value: owner,
+					inline: true,
+				},
+				{
+					name: 'Region',
+					value: region,
+					inline: true,
+				},
+				{
+					name: 'Role count',
+					value: roles.cache.size,
+					inline: true,
+				},
+				{
+					name: 'Number of Members',
+					value: memberCount,
+					inline: true,
+				},
+			)
+			.setTimestamp()
+			.setFooter('Requested by: ');
+
 		if (CMD_NAME === 'serverinfo') {
-			client.guilds.cache.forEach((guild) => {
-				if (message.guild.id === guild.id) {
-					message.channel.send(
-						`${guild.name} has a total of ${guild.memberCount} members`,
-					);
-				}
-			});
+			// client.guilds.cache.forEach((guild) => {
+			// 	if (message.guild.id === guild.id) {
+			// 		message.channel.send(
+			// 			`${guild.name} has a total of ${guild.memberCount} members`,
+			// 		);
+			// 	}
+			// });
+
+			// roles.cache.forEach((value, key) => {
+			// 	message.channel.send(`<@&${key}>`);
+			// });
+
+			message.channel.send(infoEmbed);
 		}
 	}
 };
