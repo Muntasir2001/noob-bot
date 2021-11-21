11abae182f9a989bfb9fbadecb6a831c6878a268
Muntasir <muntasirenamalhuda@gmail.com>
2021-11-21 22:32:03 +0000

    removed guild id

--- src/bot.js
+++ src/bot.js
@@ -28,7 +28,7 @@ const welcome = require('./commands/welcome');
 client.on('ready', () => {
 	console.log(`${client.user.tag} has logged in BEEP BEEP 🤖`);
 
-	const guildId = '808385971418693652';
+	const guildId = '';
 	const guild = client.guilds.cache.get(guildId);
 	let commands;
 
