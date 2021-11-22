cce53e6d954cd5528402e9c168ea02f67929e0dc
Muntasir2001 <muntasirenamalhuda@gmail.com>
2021-11-22 16:20:46 +0000

    updated command registration conditional statement in bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const { Client, Intents, Constants } = require('discord.js');
 
 const client = new Client({
-	partials: ['MESSAGE', 'REACTION'],
-	intents: [
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MESSAGES,
-		Intents.FLAGS.DIRECT_MESSAGES,
-		Intents.FLAGS.GUILD_BANS,
-		Intents.FLAGS.GUILD_MESSAGE_TYPING,
-	],
+   partials: ['MESSAGE', 'REACTION'],
+   intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.DIRECT_MESSAGES,
+      Intents.FLAGS.GUILD_BANS,
+      Intents.FLAGS.GUILD_MESSAGE_TYPING,
+   ],
 });
 
 // const basicCommands = require('./commands/basicCommands');
@@ -26,23 +26,23 @@ const welcome = require('./commands/welcome');
 
 //status of the bot
 client.on('ready', () => {
-	console.log(`${client.user.tag} has logged in BEEP BEEP 🤖`);
+   console.log(`${client.user.tag} has logged in BEEP BEEP 🤖`);
 
-	const guildId = '';
-	const guild = client.guilds.cache.get(guildId);
-	let commands;
+   const guildId = '';
+   const guild = client.guilds.cache.get(guildId);
+   let commands;
 
-	if (guild) {
-		commands = guild.commands;
-	} else {
-		commands = client.application.commands;
-	}
+   if (guild && guildId.length !== 0) {
+      commands = guild.commands;
+   } else {
+      commands = client.application.commands;
+   }
 
-	registerCommands(commands, guild, commandsList);
+   registerCommands(commands, guild, commandsList);
 });
 
 client.on('interactionCreate', async (interaction) => {
-	slashCommandHandler(interaction, client);
+   slashCommandHandler(interaction, client);
 });
 
 //message event listener - when anyone types a message/certain command in the text chat (v12)
@@ -54,36 +54,36 @@ client.on('guildMemberAdd', welcome);
 
 //add reaction roles
 client.on('messageReactionAdd', (reaction, user) => {
-	const { name } = reaction.emoji;
-	const member = reaction.message.guild.members.cache.get(user.id);
-	if (reaction.message.id === '845261229776830464') {
-		switch (name) {
-			case '🖥️':
-				member.roles.add('845297466521813043');
-				break;
+   const { name } = reaction.emoji;
+   const member = reaction.message.guild.members.cache.get(user.id);
+   if (reaction.message.id === '845261229776830464') {
+      switch (name) {
+         case '🖥️':
+            member.roles.add('845297466521813043');
+            break;
 
-			case '💻':
-				member.roles.add('845297666069233735');
-				break;
-		}
-	}
+         case '💻':
+            member.roles.add('845297666069233735');
+            break;
+      }
+   }
 });
 
 //remove roles with reactions
 client.on('messageReactionRemove', (reaction, user) => {
-	const { name } = reaction.emoji;
-	const member = reaction.message.guild.members.cache.get(user.id);
-	if (reaction.message.id === '845261229776830464') {
-		switch (name) {
-			case '🖥️':
-				member.roles.remove('845297466521813043');
-				break;
+   const { name } = reaction.emoji;
+   const member = reaction.message.guild.members.cache.get(user.id);
+   if (reaction.message.id === '845261229776830464') {
+      switch (name) {
+         case '🖥️':
+            member.roles.remove('845297466521813043');
+            break;
 
-			case '💻':
-				member.roles.remove('845297666069233735');
-				break;
-		}
-	}
+         case '💻':
+            member.roles.remove('845297666069233735');
+            break;
+      }
+   }
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
