6d243131af0670fc4fe7007d3ee87e11339a3040
Muntasir <muntasirenamalhuda@gmail.com>
2021-12-18 20:29:45 +0000

    set status legacy command fixed

--- src/legacy-commands/commands/setStatus.js
+++ src/legacy-commands/commands/setStatus.js
@@ -1,14 +1,12 @@
-const PREFIX = process.env.PREFIX;
-// const { client } = require('../bot');
-
 const setStatus = (message, CMD_NAME, args, client) => {
 	if (CMD_NAME === 'setstatus') {
 		if (message.author.id === '374230181889572876') {
 			client.user.setPresence({
-				activity: {
-					name: args[0],
-					type: 0,
-				},
+				activities: [
+					{
+						name: args[0],
+					},
+				],
 			});
 		} else {
 			message.channel.send(
