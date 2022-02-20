2205c22473722c5cf03a6aaf494d15d8de2f6ba3
Muntasir <muntasirenamalhuda@gmail.com>
2022-02-20 15:31:31 +0000

    wrapped movemssg in try catch

--- src/discord-utils.js
+++ src/discord-utils.js
@@ -2,125 +2,141 @@ const { array } = require('./utils');
 const { MAX_MESSAGES_FETCH, BULK_MESSAGES_LIMIT } = require('./constants');
 
 const getMessagesInRange = async (channel, start, end) => {
-   if (start.createdTimestamp > end.createdTimestamp) {
-      const temp = start;
-      start = end;
-      end = temp;
-   }
+	try {
+		if (start.createdTimestamp > end.createdTimestamp) {
+			const temp = start;
+			start = end;
+			end = temp;
+		}
 
-   let stoppedEarly = true;
-   const msgs = [start];
-   while (msgs.length < MAX_MESSAGES_FETCH) {
-      const fetchedMsgs = Array.from(
-         (
-            await channel.messages.fetch({
-               // cannot also provide the "before: end.id" option since multiple options are not supported by the API
-               after: start.id,
-               limit: BULK_MESSAGES_LIMIT,
-            })
-         ).values()
-      ).reverse(); // reverse so the messages are ordered chronologically
+		let stoppedEarly = true;
+		const msgs = [start];
+		while (msgs.length < MAX_MESSAGES_FETCH) {
+			const fetchedMsgs = Array.from(
+				(
+					await channel.messages.fetch({
+						// cannot also provide the "before: end.id" option since multiple options are not supported by the API
+						after: start.id,
+						limit: BULK_MESSAGES_LIMIT,
+					})
+				).values(),
+			).reverse(); // reverse so the messages are ordered chronologically
 
-      const indexOfEndMsg = fetchedMsgs.findIndex((msg) => msg.id === end.id);
+			const indexOfEndMsg = fetchedMsgs.findIndex(
+				(msg) => msg.id === end.id,
+			);
 
-      if (indexOfEndMsg === -1) {
-         // haven't reached the end message yet, so add messages and keep fetching for more
-         msgs.push(...fetchedMsgs);
-         start = fetchedMsgs[fetchedMsgs.length - 1];
-      } else {
-         // found the end message, so add messages (ignoring ones after end message) and stop fetching
-         msgs.push(...fetchedMsgs.slice(0, indexOfEndMsg + 1));
-         stoppedEarly = false;
-         break;
-      }
-   }
-   return [msgs, stoppedEarly];
+			if (indexOfEndMsg === -1) {
+				// haven't reached the end message yet, so add messages and keep fetching for more
+				msgs.push(...fetchedMsgs);
+				start = fetchedMsgs[fetchedMsgs.length - 1];
+			} else {
+				// found the end message, so add messages (ignoring ones after end message) and stop fetching
+				msgs.push(...fetchedMsgs.slice(0, indexOfEndMsg + 1));
+				stoppedEarly = false;
+				break;
+			}
+		}
+		return [msgs, stoppedEarly];
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const findMessageInGuild = async (messageId, guild, startingChannel) => {
-   if (startingChannel) {
-      try {
-         const foundMsg = await startingChannel.messages.fetch(messageId);
-         return [foundMsg, startingChannel];
-      } catch (err) {
-         // Do nothing
-      }
-   }
-   // TODO: Search threads as well
-   // console.log(await guild.channels.fetch());
-   const channels = Array.from((await guild.channels.fetch()).values());
-   for (let i = 0; i < channels.length; i++) {
-      const channel = channels[i];
+	try {
+		if (startingChannel) {
+			try {
+				const foundMsg = await startingChannel.messages.fetch(messageId);
+				return [foundMsg, startingChannel];
+			} catch (err) {
+				// Do nothing
+			}
+		}
+		// TODO: Search threads as well
+		// console.log(await guild.channels.fetch());
+		const channels = Array.from((await guild.channels.fetch()).values());
+		for (let i = 0; i < channels.length; i++) {
+			const channel = channels[i];
 
-      if (!channel.isText() || channel === startingChannel) continue;
+			if (!channel.isText() || channel === startingChannel) continue;
 
-      try {
-         const foundMsg = await channel.messages.fetch(messageId);
-         return [foundMsg, channel];
-      } catch (err) {
-         // Do nothing
-      }
-   }
+			try {
+				const foundMsg = await channel.messages.fetch(messageId);
+				return [foundMsg, channel];
+			} catch (err) {
+				// Do nothing
+			}
+		}
 
-   return [];
+		return [];
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const getInfoFromCommandInteraction = async (interaction, options = {}) => {
-   //haven't understood bottom two lines
-   const { ephemeral = false } = options;
-   const interactionMsg = !ephemeral ? await interaction.fetchReply() : null;
+	try {
+		// DOUBT: haven't understood bottom two lines
+		const { ephemeral = false } = options;
+		const interactionMsg = !ephemeral ? await interaction.fetchReply() : null;
 
-   // Guild
-   if (interaction.inGuild()) {
-      const channel = await interaction.guild.channels.fetch(
-         interaction.channelId
-      );
-      if (!channel || !channel.isText()) {
-         return {
-            message: null,
-            channel: null,
-            author: null,
-         };
-      }
-      const message = interactionMsg
-         ? await channel.messages.fetch(interactionMsg.id)
-         : null;
-      const member = await interaction.guild.members.fetch(interaction.user.id);
-      const author = member.user;
+		// Guild
+		if (interaction.inGuild()) {
+			const channel = await interaction.guild.channels.fetch(
+				interaction.channelId,
+			);
+			if (!channel || !channel.isText()) {
+				return {
+					message: null,
+					channel: null,
+					author: null,
+				};
+			}
+			const message = interactionMsg
+				? await channel.messages.fetch(interactionMsg.id)
+				: null;
+			const member = await interaction.guild.members.fetch(
+				interaction.user.id,
+			);
+			const author = member.user;
 
-      return {
-         channel,
-         message,
-         author,
-      };
-   }
+			return {
+				channel,
+				message,
+				author,
+			};
+		}
 
-   // DM
-   const channel = await client.channels.fetch(interaction.channelId);
-   const author = interaction.user;
-   const message = interactionMsg
-      ? await channel.messages.fetch(interactionMsg.id)
-      : null;
-   return {
-      channel,
-      message,
-      author,
-   };
+		// DM
+		const channel = await client.channels.fetch(interaction.channelId);
+		const author = interaction.user;
+		const message = interactionMsg
+			? await channel.messages.fetch(interactionMsg.id)
+			: null;
+		return {
+			channel,
+			message,
+			author,
+		};
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const usersHavePermission = (channel, userOrUsers, permission) => {
-   const users = array(userOrUsers);
-   // DOUBT: no clue what is this if statement for
-   if (!('permissionsFor' in channel)) return true;
+	const users = array(userOrUsers);
+	// DOUBT: no clue what is this if statement for
+	if (!('permissionsFor' in channel)) return true;
 
-   return users.every((user) =>
-      Boolean(channel.permissionsFor(user)?.has(permission))
-   );
+	return users.every((user) =>
+		Boolean(channel.permissionsFor(user)?.has(permission)),
+	);
 };
 
 module.exports = {
-   getMessagesInRange,
-   findMessageInGuild,
-   getInfoFromCommandInteraction,
-   usersHavePermission,
+	getMessagesInRange,
+	findMessageInGuild,
+	getInfoFromCommandInteraction,
+	usersHavePermission,
 };
--- src/slash-commands/commands/moveMessages.js
+++ src/slash-commands/commands/moveMessages.js
@@ -1,184 +1,167 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+// const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 const {
-   getMessagesInRange,
-   usersHavePermission,
-   findMessageInGuild,
-   getInfoFromCommandInteraction,
+	getMessagesInRange,
+	usersHavePermission,
+	findMessageInGuild,
+	getInfoFromCommandInteraction,
 } = require('../../discord-utils');
 const { filterOutFalsy } = require('../../utils');
 
-const moveMessage = async (channel, message, replyTo) => {
-   await channel.sendTyping();
+const moveMessage = async (channel, message) => {
+	await channel.sendTyping();
 
-   const newMessageEmbed = new MessageEmbed({
-      author: {
-         name: message.author.username,
-         icon_url: message.author.avatarURL() || undefined,
-      },
-      description: message.content,
-   });
+	const newMessageEmbed = new MessageEmbed({
+		author: {
+			name: message.author.username,
+			icon_url: message.author.avatarURL() || undefined,
+		},
+		description: message.content,
+	});
 
-   const newMessageArgs = {
-      embeds: [newMessageEmbed].concat(message.embeds),
-      files: Array.from(message.attachments.values()),
-   };
+	const newMessageArgs = {
+		embeds: [newMessageEmbed].concat(message.embeds),
+		files: Array.from(message.attachments.values()),
+	};
 
-   const newMessage = await (replyTo
-      ? replyTo.reply(newMessageArgs)
-      : channel.send(newMessageArgs));
-   await message.delete();
+	const newMessage = await channel.send(newMessageArgs);
+	await message.delete();
 
-   return newMessage;
+	return newMessage;
 };
 
 const moveMessages = async (interaction, CMD_NAME, options, client) => {
-   const channelId = options.getChannel('to_channel').id;
-   const startId = options.getString('start_message_id');
-   const endId = options.getString('end_message_id');
+	try {
+		const channelId = options.getChannel('to_channel').id;
+		const startId = options.getString('start_message_id');
+		const endId = options.getString('end_message_id');
 
-   console.log('to channel ID: ', channelId);
+		console.log('to channel ID: ', channelId);
 
-   const toChannel = await interaction.guild.channels.fetch(channelId);
+		const toChannel = await interaction.guild.channels.fetch(channelId);
 
-   if (!toChannel || !toChannel.isText()) {
-      interaction.reply(`Could not resolve toChannel: <#${channelId}>`);
-      return null;
-   }
+		if (!toChannel || !toChannel.isText()) {
+			interaction.reply(`Could not resolve toChannel: <#${channelId}>`);
+			return null;
+		}
 
-   const [startMsg, fromChannel] = await findMessageInGuild(
-      startId,
-      interaction.guild,
-      interaction.channel
-   );
+		const [startMsg, fromChannel] = await findMessageInGuild(
+			startId,
+			interaction.guild,
+			interaction.channel,
+		);
 
-   if (!startMsg || !fromChannel) {
-      await interaction.reply('Could not find starting message.');
-      return null;
-   }
+		if (!startMsg || !fromChannel) {
+			await interaction.reply('Could not find starting message.');
+			return null;
+		}
 
-   if (toChannel.id === fromChannel.id) {
-      await interaction.reply("You're moving messages to the same channel??");
-      return null;
-   }
+		if (toChannel.id === fromChannel.id) {
+			await interaction.reply(
+				"You're moving messages to the same channel??",
+			);
+			return null;
+		}
 
-   const { author } = await getInfoFromCommandInteraction(interaction, {
-      ephemeral: true,
-   });
+		const { author } = await getInfoFromCommandInteraction(interaction, {
+			ephemeral: true,
+		});
 
-   if (!author) {
-      await interaction.reply('Could not find who is invoking this command.');
-      return null;
-   }
+		if (!author) {
+			await interaction.reply(
+				'Could not find who is invoking this command.',
+			);
+			return null;
+		}
 
-   const authorAndBot = filterOutFalsy([author, client.user]);
+		const authorAndBot = filterOutFalsy([author, client.user]);
 
-   // INFO: permission
-   if (
-      !usersHavePermission(toChannel, authorAndBot, [
-         'SEND_MESSAGES',
-         'VIEW_CHANNEL',
-      ])
-   ) {
-      await interaction.reply(
-         `One of us not have access to send messages in <#${toChannel.id}>`
-      );
-      return null;
-   }
+		// INFO: permission
+		if (
+			!usersHavePermission(toChannel, authorAndBot, [
+				'SEND_MESSAGES',
+				'VIEW_CHANNEL',
+			])
+		) {
+			await interaction.reply(
+				`One of us not have access to send messages in <#${toChannel.id}>`,
+			);
+			return null;
+		}
 
-   if (
-      !usersHavePermission(fromChannel, authorAndBot, [
-         'MANAGE_MESSAGES',
-         'VIEW_CHANNEL',
-      ])
-   ) {
-      await interaction.reply(
-         `One of us not have access to delete messages in <#${fromChannel.id}>`
-      );
-      return null;
-   }
+		if (
+			!usersHavePermission(fromChannel, authorAndBot, [
+				'MANAGE_MESSAGES',
+				'VIEW_CHANNEL',
+			])
+		) {
+			await interaction.reply(
+				`One of us not have access to delete messages in <#${fromChannel.id}>`,
+			);
+			return null;
+		}
 
-   // INFO: single message; not a range
-   if (!endId) {
-      await toChannel.sendTyping();
-      await toChannel.send(`__1 message moved from__ <#${fromChannel.id}>`);
+		// INFO: single message; not a range
+		if (!endId) {
+			await toChannel.sendTyping();
+			await toChannel.send(`__1 message moved from__ <#${fromChannel.id}>`);
 
-      await moveMessage(toChannel, startMsg);
+			await moveMessage(toChannel, startMsg);
 
-      await interaction.reply({ content: '1 message moved.' });
+			await interaction.reply({ content: '1 message moved.' });
 
-      return null;
-   }
+			return null;
+		}
 
-   // INFO: for more than one message
+		// INFO: for more than one message
+		let endMsg;
 
-   let endMsg;
+		try {
+			endMsg = await fromChannel.messages.fetch(endId);
+		} catch (err) {
+			await interaction.reply(
+				'End message is not in the same channel as start message.',
+			);
 
-   try {
-      endMsg = await fromChannel.messages.fetch(endId);
-   } catch (err) {
-      await interaction.reply(
-         'End message is not in the same channel as start message.'
-      );
+			return null;
+		}
 
-      return null;
-   }
+		const [msgs, stoppedEarly] = await getMessagesInRange(
+			fromChannel,
+			startMsg,
+			endMsg,
+		);
 
-   const [msgs, stoppedEarly] = await getMessagesInRange(
-      fromChannel,
-      startMsg,
-      endMsg
-   );
+		toChannel.sendTyping();
+		await toChannel.send(
+			`__${msgs.length} messages moved from__ <#${fromChannel.id}>`,
+		);
 
-   toChannel.sendTyping();
-   await toChannel.send(
-      `__${msgs.length} messages moved from__ <#${fromChannel.id}>`
-   );
+		await interaction.reply('Working on it');
 
-   await interaction.reply('Working on it');
+		// INFO: old message in old channel to new message in new channel mapping
+		const oldToNewMessageMapping = {};
+		for (let i = 0; i < msgs.length; i++) {
+			const msg = msgs[i];
+			const newMessage = await moveMessage(toChannel, msg);
 
-   // DOUBT: no idea what is this for
-   const oldToNewMessageMapping = {};
-   for (let i = 0; i < msgs.length; i++) {
-      const msg = msgs[i];
-      const newMessage = await moveMessage(
-         toChannel,
-         msg
-         // msg.reference.messageId
-         //    ? oldToNewMessageMapping[msg.reference.messageId]
-         //    : undefined
-      );
+			oldToNewMessageMapping[msg.id] = newMessage;
+		}
 
-      oldToNewMessageMapping[msg.id] = newMessage;
-   }
-
-   await interaction.editReply(
-      `Moved ${msgs.length} messages. ${
-         stoppedEarly
-            ? '\nNote: Some messages in the range were not included due to a rate limit precaution.'
-            : ''
-      }`
-   );
-
-   // INFO: these are not needed, but need to do some refactoring
-   // const confirmPrompt = `Are you sure you want to move ${
-   //    msgs.length
-   // } messages to <#${toChannel.id}>?${
-   //    stoppedEarly
-   //       ? '\nNote: Some messages in the range were not included due to a rate limit precaution.'
-   //       : ''
-   // }`;
-
-   // const workingMessage = `Moving ${msgs.length} messages to <#${toChannel.id}>...`;
-
-   // const res = {
-   //    intermediateResult: { msgs, toChannel, fromChannel },
-   //    confirmPrompt,
-   //    workingMessage,
-   // };
-
-   // return res;
+		await interaction.editReply(
+			`Moved ${msgs.length} messages. ${
+				stoppedEarly
+					? '\nNote: Some messages in the range were not included due to a rate limit precaution.'
+					: ''
+			}`,
+		);
+	} catch (error) {
+		console.log(error);
+		await interaction.reply(
+			'Error: could be either invalid message ID or channel names',
+		);
+	}
 };
 
 module.exports = moveMessages;
