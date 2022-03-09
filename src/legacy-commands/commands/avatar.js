9a5ec3a278ed8ad201cbbcc4bc5fee960ce83a57
Muntasir <muntasirenamalhuda@gmail.com>
2022-03-09 22:41:18 +0000

    added avatar legacy command

--- src/legacy-commands/commands/avatar.js
+++ src/legacy-commands/commands/avatar.js
@@ -3,11 +3,45 @@ const { MessageEmbed } = require('discord.js');
 const getMember = require('../utilities/getMember');
 
 const avatar = async (message, CMD_NAME, args, client) => {
-   const user =
-      message.mentions.members.first().displayAvatarURL() ||
-      (await getMember(client, args[0], message.channel));
+	let avatar;
+	let user;
 
-   console.log(user);
+	if (message.mentions.members.first()) {
+		user = message.mentions.members.first().user;
+		avatar = message.mentions.members
+			.first()
+			.displayAvatarURL({ size: 2048 });
+	} else {
+		user = await getMember(
+			client,
+			args[0] || message.member.user,
+			message.channel,
+			true,
+		);
+		avatar = user.displayAvatarURL({ size: 2048 });
+	}
+
+	const avatarEmbed = new MessageEmbed()
+		.setColor(user.hexAccentColor || '#FF4454')
+		.setTitle(`Avatar for ${user.tag}`)
+		.setDescription(
+			`[jpg](${user.displayAvatarURL({
+				format: 'jpg',
+				size: 4096,
+			})}) | [png](${user.displayAvatarURL({
+				format: 'png',
+				size: 4096,
+			})}) | [webp](${user.displayAvatarURL({
+				format: 'webp',
+				size: 4096,
+			})}) | [jpeg](${user.displayAvatarURL({
+				format: 'jpeg',
+				size: 4096,
+			})})`,
+		)
+		.setImage(avatar);
+
+	message.channel.send({ embeds: [avatarEmbed] });
 };
 
 module.exports = avatar;
--- src/legacy-commands/utilities/getMember.js
+++ src/legacy-commands/utilities/getMember.js
@@ -1,14 +1,16 @@
-const getMember = async (client, userID, channel) => {
-   try {
-      const user = await client.users.fetch(userID).catch((err) => {
-         channel.send(`${userID} is an unknown user`);
-         console.log(err);
-      });
+const getMember = async (client, userID, channel, force) => {
+	try {
+		const user = await client.users
+			.fetch(userID, { force: force })
+			.catch((err) => {
+				channel.send(`${userID} is an unknown user`);
+				console.log(err);
+			});
 
-      return user;
-   } catch (errr) {
-      return;
-   }
+		return user;
+	} catch (errr) {
+		return;
+	}
 };
 
 module.exports = getMember;
