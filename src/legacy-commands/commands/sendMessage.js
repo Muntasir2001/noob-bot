ce706793653b26d0ea5c128d536767cdd28c92ed
Muntasir <muntasirenamalhuda@gmail.com>
2022-05-15 16:58:16 +0100

    removed if statement check for commandname in send message

--- src/legacy-commands/commands/sendMessage.js
+++ src/legacy-commands/commands/sendMessage.js
@@ -1,40 +1,38 @@
 const getTextChannel = require('../utilities/getTextChannel');
 const infoMessageEmbed = require('../../globalUtils/infoMessageEmbed');
 const sendMessage = async (message, CMD_NAME, args) => {
-	if (CMD_NAME === 'sm') {
-		if (!args[0]) {
-			return message.reply({
-				embeds: [
-					infoMessageEmbed('Please provide channel ID or tag a channel'),
-				],
-			});
-		}
+	if (!args[0]) {
+		return message.reply({
+			embeds: [
+				infoMessageEmbed('Please provide channel ID or tag a channel'),
+			],
+		});
+	}
 
-		if (!args[1]) {
-			return message.reply({
-				embeds: [infoMessageEmbed('Please provide a message')],
-			});
-		}
+	if (!args[1]) {
+		return message.reply({
+			embeds: [infoMessageEmbed('Please provide a message')],
+		});
+	}
 
-		let channel;
+	let channel;
 
-		if (args[0].charAt(0) === '<' && args[0].charAt(1) === '#') {
-			channel = await getTextChannel(args[0].slice(2).slice(0, -1), message);
-		} else {
-			channel = await getTextChannel(args[0], message);
-		}
+	if (args[0].charAt(0) === '<' && args[0].charAt(1) === '#') {
+		channel = await getTextChannel(args[0].slice(2).slice(0, -1), message);
+	} else {
+		channel = await getTextChannel(args[0], message);
+	}
 
-		args.shift();
+	args.shift();
 
-		if (channel !== null) {
-			return channel.send(args.join(' '));
-		} else {
-			return message.reply({
-				embeds: [
-					infoMessageEmbed('Invalid channel ID or something went wrong!'),
-				],
-			});
-		}
+	if (channel !== null) {
+		return channel.send(args.join(' '));
+	} else {
+		return message.reply({
+			embeds: [
+				infoMessageEmbed('Invalid channel ID or something went wrong!'),
+			],
+		});
 	}
 };
 
