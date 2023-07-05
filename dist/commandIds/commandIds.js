6c8f701ad4e7a2ffe6732c7d073133cbd152a22b
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-07-05 15:55:49 +0100

    udpated prod command id

--- dist/commandIds/commandIds.js
+++ dist/commandIds/commandIds.js
@@ -2,32 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const commandIds = {
     HELP: process.env.MODE === 'DEV' ? '1121851980164386826' : '912108220917612596',
-    KICK: process.env.MODE === 'DEV'
-        ? '1121047119072067605'
-        : '1121047119072067605',
+    KICK: process.env.MODE === 'DEV' ? '1121047119072067605' : '912309487413637120',
     WARN: process.env.MODE === 'DEV'
         ? '1125838943779307631'
-        : '1125838943779307631',
-    BAN: process.env.MODE === 'DEV'
-        ? '1121047118577147907'
-        : '1121047118577147907',
-    TIMEOUT: process.env.MODE === 'DEV'
-        ? '1121047119072067608'
-        : '1121047119072067608',
+        : '1028216253338357760',
+    BAN: process.env.MODE === 'DEV' ? '1121047118577147907' : '912309488164433970',
+    TIMEOUT: process.env.MODE === 'DEV' ? '1121047119072067608' : '951799116797718550',
     PURGE: process.env.MODE === 'DEV'
         ? '1121047119072067607'
-        : '1121047119072067607',
-    SERVER_INFO: process.env.MODE === 'DEV'
-        ? '1121478569583988816'
-        : '1121478569583988816',
-    BOT_INFO: process.env.MODE === 'DEV'
-        ? '1121047118577147906'
-        : '1121047118577147906',
-    USER_INFO: process.env.MODE === 'DEV'
-        ? '1121486667165999204'
-        : '1121486667165999204',
-    AVATAR: process.env.MODE === 'DEV'
-        ? '1121474025030422608'
-        : '1121474025030422608',
+        : '1126163379225112606',
+    SERVER_INFO: process.env.MODE === 'DEV' ? '1121478569583988816' : '912309572339920906',
+    BOT_INFO: process.env.MODE === 'DEV' ? '1121047118577147906' : '912309659019382794',
+    USER_INFO: process.env.MODE === 'DEV' ? '1121486667165999204' : '912309488927785000',
+    AVATAR: process.env.MODE === 'DEV' ? '1121474025030422608' : '951451554509119558',
 };
 exports.default = commandIds;
